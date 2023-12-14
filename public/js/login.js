19055b3adfd4ced0dbbf5f55d290e5e98bcb3843
Ajay S <ajaycshan@gmail.com>
2023-12-14 21:58:38 +1100

    Create views, styling and login functionality

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+const login = async (event) => {
+	event.preventDefault();
+
+	const username = document.querySelector('#login-username').value.trim();
+	const password = document.querySelector('#login-password').value.trim();
+
+	if (username && password) {
+		const response = await fetch('/api/user/login', {
+			method: 'POST',
+			body: JSON.stringify({ username, password }),
+			headers: { 'Content-Type': 'application/json' },
+		});
+
+		if (response.ok) {
+			document.location.replace('/dashboard');
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+document.querySelector('#login-button').addEventListener('click', login);
