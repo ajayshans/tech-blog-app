88c04b339c63825260368dac3dbb718531ed0433
Ajay S <ajaycshan@gmail.com>
2023-12-14 19:35:15 +1100

    Add user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,10 +2,62 @@ const router = require('express').Router();
 // Import User model
 const { User } = require('../../models');
 
-// TODO: Add user routes
+// Request to create a new user and log in to newly created account from signup page
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
 
-// POST (create user /) - request to create a user and log in to newly created account
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
 
-// POST (/login) - request to log into existing account
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// POST (/logout) - request to log out of account
+// Request to log into existing account and save user data in session
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+// Request to log out of account and deleting the user's data from the session
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
